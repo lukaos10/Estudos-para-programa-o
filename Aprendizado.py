bc19f2ebcacc8e27fcfc8c25f63b926414501a42
lukaos10 <117240514+lukaos10@users.noreply.github.com>
2024-12-04 17:26:41 +0000

    Meu novo código

--- Aprendizado.py
+++ Aprendizado.py
@@ -1,1 +1,9 @@
-print ("Olá mundo")
+def numero_de_alunos (alunos):
+    if alunos >= 1000:
+        print("Precisamos construir uma escola maior")
+    else:
+        print ("Ta de boa")
+
+alunos_numero = numero_de_alunos(10000)
+
+print (alunos_numero)
