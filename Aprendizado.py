a44bd9e63685d9e17643132bc060b7812fb0beda
lukaos10 <117240514+lukaos10@users.noreply.github.com>
2024-12-11 19:21:06 +0000

    Aprendizados de classes e objetos em python

--- Aprendizado.py
+++ Aprendizado.py
@@ -1,9 +1,32 @@
-def numero_de_alunos (alunos):
-    if alunos >= 1000:
-        print("Precisamos construir uma escola maior")
-    else:
-        print ("Ta de boa")
+class Usuario:
 
-alunos_numero = numero_de_alunos(10000)
+        def __init__(self, nome, email, idade):
+                self.nome = nome
+                self.email = email
+                self.idade = idade
 
-print (alunos_numero)
+
+        def saudacao(self):
+                print (f"Olá meu nome é {self.nome}, eu tenho {self.idade} anos.")
+
+        def aniversario(self):
+                self.idade += 1
+
+
+class Cliente(Usuario):
+
+        def __init__(self, nome, email, idade, saldo):
+                Usuario.__init__(self,nome, email, idade)
+                self.saldo = saldo
+
+        def saudacao(self):
+                print (f"Olá meu nome é {self.nome}, eu tenho {self.idade} anos, minha conta bancario tem {self.saldo} de money")
+
+
+cliente1 = Cliente("Carlos", "carlos@gmail.com", 18, 1000)
+cliente1.saudacao()
+print(cliente1.saldo)
+
+
+usuario1 = Usuario("Pedro","pedro@gmail.com", 30)
+usuario1.saudacao()
